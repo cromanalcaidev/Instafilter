786a96dfb1ed65ab1b70e378762f2c67a7bd07e2
Carlos Román Alcaide <cromanalcaidev@gmail.com>
2024-05-09 16:17:26 +0200

    Initial commit

---
+++ Instafilter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Instafilter
+//
+//  Created by Carlos Román Alcaide on 6/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+    var body: some View {
+        VStack{
+            Text("Hello")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Carlos Román Alcaide on 6/5/24.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
